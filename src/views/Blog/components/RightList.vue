ae0b6a6aae69c88d476cbc0693aa552428da0113
李洁 <2977290894@qq.com>
2023-10-08 21:25:50 +0800

    blog-right

---
+++ src/views/Blog/components/RightList.vue
@@ -0,0 +1,50 @@
+<template>
+  <div class="right-container">
+    <ul>
+        <li  v-for="(item ,i) in list" :key="i">
+        <span @click="handleChang(item)" :class="{active:item.isSelect}">{{ item.name }}</span>
+        <RightList :list="item.children" @Selected="handleChang"/>
+        </li>
+        
+    </ul>
+  </div>
+</template>
+
+<script>
+
+export default {
+    name: "RightList",
+    props: {
+        list: {
+            type: Array,
+            default: () => { []; }
+        }
+    },
+    methods: {
+        handleChang(item) {
+            this.$emit("Selected", item);
+        }
+    }
+}
+</script>
+
+<style scoped lang="less">
+@import "~@/styles/var.less";
+    .right-container{
+       
+        li{
+            list-style: none;
+            padding: 0;
+            margin-left: 15px;
+            min-height: 40px;
+            line-height: 40px;
+            
+            span{
+                cursor: pointer;
+            }
+        }
+        .active{
+            color: @warn;
+        }
+    }
+</style>
---
+++ src/views/Blog/components/RightTest.vue
@@ -0,0 +1,87 @@
+<template>
+  <div class="app-container">
+    <RightList :list="list" @Selected="handled"/>
+  </div>
+</template>
+
+<script>
+import RightList from '@/views/Blog/components/RightList.vue';
+export default {
+components:{
+    RightList
+},
+data(){
+return {
+    list:[{
+        name:"a",
+        isSelect:false
+    },
+    {
+        name:"b",
+        isSelect:true,
+        children:[
+            {
+                name:"b-1",
+        isSelect:false
+            },
+            {
+                name:"b-2",
+                children:[
+            {
+                name:"b-1",
+        isSelect:false
+            },
+            {
+                name:"b-2",
+        isSelect:false,
+            },
+            {
+                name:"b-3",
+        isSelect:false
+            },
+        ],
+        isSelect:false,
+            },
+            {
+                name:"b-3",
+        isSelect:false
+            },
+        ]
+    },
+    {
+        name:"a",
+        isSelect:false,
+        children:[
+            {
+                name:"c-1",
+        isSelect:false
+            },
+            {
+                name:"b-2",
+        isSelect:false
+            },
+            {
+                name:"b-3",
+        isSelect:false
+            },
+        ]
+    },
+    {
+        name:"a",
+        isSelect:false
+    },
+]
+}
+},
+methods:{
+    handled(){
+        console.log(1);
+    }
+}
+
+}
+</script>
+
+<style>
+
+</style>
